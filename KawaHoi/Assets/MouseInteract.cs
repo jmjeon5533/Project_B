82bac0e0bd5431a346be8f1d8c00e63c9d55689f
jmjeon5533 <jmjeon5533@gmail.com>
2025-01-22 19:53:30 +0900

    Raycast Navigation 이동 제작

--- KawaHoi/Assets/MouseInteract.cs
+++ KawaHoi/Assets/MouseInteract.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class MouseInteract : MonoBehaviour
 {
+    public Unit selectUnit;
     RaycastHit hit;
     void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, int.MaxValue, LayerMask.GetMask("Environment")))
+            if (Physics.Raycast(ray, out hit, int.MaxValue, LayerMask.GetMask("Environment")))
             {
                 print("Interact");
+                selectUnit.TargetMove(hit.point);
             }
         }
     }
