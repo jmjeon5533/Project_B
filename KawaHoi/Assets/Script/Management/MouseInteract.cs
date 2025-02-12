e46298ce39eb37d4e80dc2f945bf43c19f3a071b
jmjeon5533 <jmjeon5533@gmail.com>
2025-02-12 21:20:21 +0900

    Unit 선택 기능

--- KawaHoi/Assets/Script/Management/MouseInteract.cs
+++ KawaHoi/Assets/Script/Management/MouseInteract.cs
@@ -5,18 +5,54 @@ public class MouseInteract : MonoBehaviour
 {
     public Unit selectUnit;
     RaycastHit hit;
+    int layerMask;
+    private void Start()
+    {
+        layerMask = LayerMask.GetMask("Environment", "Unit");
+    }
     void Update()
     {
         if (!StageManager.instance.isGame) return;
         if (Input.GetMouseButtonUp(0))
         {
             if (EventSystem.current.IsPointerOverGameObject()) return;
-            
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, int.MaxValue, LayerMask.GetMask("Environment")))
+            if (Physics.Raycast(ray, out hit, int.MaxValue, layerMask))
             {
-                print(hit.point);
-                selectUnit.TargetMove(hit.point);
+                GameObject hitObject = hit.collider.gameObject;
+                int hitLayer = hitObject.layer;
+
+                switch (hitLayer)
+                {
+                    case int _ when hitLayer == LayerMask.NameToLayer("Environment") && selectUnit != null:
+                        // 환경을 클릭했을 때 이동
+                        selectUnit = null;
+                        break;
+                    case int _ when hitLayer == LayerMask.NameToLayer("Unit"):
+                        selectUnit = hitObject.GetComponent<Unit>();
+                        break;
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, int.MaxValue, layerMask))
+            {
+                GameObject hitObject = hit.collider.gameObject;
+                int hitLayer = hitObject.layer;
+
+                switch (hitLayer)
+                {
+                    case int _ when hitLayer == LayerMask.NameToLayer("Environment") && selectUnit != null:
+                        // 환경을 클릭했을 때 이동
+                        selectUnit.TargetMove(hit.point);
+                        break;
+                }
+
             }
         }
     }
